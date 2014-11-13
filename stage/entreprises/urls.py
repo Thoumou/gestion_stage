272b22491073414619b147fa6f267270b947fa4e
Thoumou <gauvin.th@gmail.com>
2014-11-13 09:59:18 +0100

    Fusion du template et de la liste des entreprises

--- stage/entreprises/urls.py
+++ stage/entreprises/urls.py
@@ -3,8 +3,8 @@ from entreprises import views
 
 urlpatterns = patterns("",
     url(r'^$', views.index, name='index'),
-    url(r'^list/$', views.ent_listing, name="list"),
-    url(r'^entreprises/?$'                   , views.liste_entreprises, name="liste-entreprises"     ),
-    url(r'^entreprises/(?P<page>\d+)/?$'     , views.liste_entreprises, name="liste-entreprises-page"),
+    url(r'^(?P<page>\d+)/?$', views.index, name='index-page'),
+    # url(r'^entreprises/?$'                   , views.liste_entreprises, name="liste-entreprises"     ),
+    # url(r'^entreprises/(?P<page>\d+)/?$'     , views.liste_entreprises, name="liste-entreprises-page"),
 	#url(r'^entreprises/detail/(?P<id>\d+)/?$', views.detail_entreprise, name="detail-entreprise"     ),
 )
